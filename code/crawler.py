1433efe8b49a069c4dac51b0a1a3297babb57712
yoayo112 <yoayo112@gmail.com>
2024-04-03 00:53:08 -0600

    cleaned up list formatting. include following comment lines. respect 2000 char limit (split into multiple messages)

--- code/crawler.py
+++ code/crawler.py
@@ -13,7 +13,10 @@ import discord
 
 #For now I guess just paste discord server token here when you want to run the bot. oh I guess I also need to auth github.
 
+
+
 repoPath = 'yoayo112/Change-The-Game'
+ignore = ['BrushBase.cs','PaletteWindow.cs', 'TMP_TextInfoDebugTool.cs']
 #Something like this???
 
 
@@ -38,49 +41,82 @@ def handle_user_messages(msg) ->str:
 
         #creating a search query to give to git hubs very convenient search api
         query = "TODO+in:file+extension:cs+repo:yoayo112/Change-The-Game"
-        todolist = []
+        #dictionary to store todo comments
+        l = 0
+        todolist = [{}]
+        #master character count
+        charCount = 0
+        #search the code
         contents = requests.get('http://api.github.com/search/code?q='+query, headers=headers).json()
         hits = contents["items"]
         for hit in hits: #json file .items is the list of results.
             #  :(
             #  turns out, all Im gonna get from this is another url 
             #So I guess we have to step into it?
-            githubURL = create_github_rawurl(hit["html_url"])
-            print(githubURL)
-            try:
-                #try stepping into the diff html
-                response = urlopen(githubURL)
-                # read the code into a string
-                code = response.read().decode('utf-8')
-                #if it worked .. we should have a bigass string
-                lines = code.split("\n")
-                for line in lines:
-                    if "TODO" in line:
-                        todolist.append(line)
-                
-            except: 
-                print("fail!!!")
-                pass
-			    # raise Exception, "failed to open modified GitHub URL " + githubURL
+            name = str(hit["name"])
+            if name not in ignore:
+                if charCount >= 1900:
+                    l += 1
+                    todolist.append({})
+                    charCount = 0
+                githubURL = create_github_rawurl(hit["html_url"])
+                print(githubURL)
+                try:
+                    #try stepping into the diff html
+                    response = urlopen(githubURL)
+                    # read the code into a string
+                    code = response.read().decode('utf-8')
+                    #if it worked .. we should have a bigass string
+                    lines = code.split("\n")
+                    previousLineWas_TODO = False
+                    previousLineWas = -1
+                    i = 1
+                    for line in lines:
 
+                        if "TODO" in line:
+                            todolist[l].update({ "In "+name+" on line "+str(i) : line +'\n'})
+                            previousLineWas_TODO = True
+                            previousLineWas = i
+                            charCount = charCount + 16 + len(name) + len(line) # accounting for the list number i.e. '1.)'
+
+                        elif "//" in line and previousLineWas_TODO == True and previousLineWas != -1: 
+                            oldLine = todolist[l]["In "+name+" on line "+str(previousLineWas)]
+                            todolist[l]["In "+name+" on line "+str(previousLineWas)] = oldLine + line + '\n'
+                            charCount = charCount + 13 + len(name) + len(line)
+
+                        elif "//" not in line and previousLineWas_TODO == True:
+                            previousLineWas_TODO = False
+                            previousLineWas = -1
+
+                        i += 1
+                
+                except: 
+                    print("fail!!!")
+                    pass
+			        # raise Exception, "failed to open modified GitHub URL " + githubURL
+            #
         #in theory todoList now contains all the hits of TODO
         return todolist
     
 
 #on message event passes content to handler -> passes the reponse back to discord.
-async def process_message(message, userMessage):
+async def process_message(client, userMessage):
     try:
         response = handle_user_messages(userMessage)
         if(not response):
             return
         else:
-            #-- see if you can set what channel it posts in!!
-            #-- see if you can call the other todo bot???
-            post = ''
-            for line in response:
-                post += line + "\n"
+            i = 1
+            for todolist in response:
+                post = ''
+                for line in todolist:
+                    post += str(i)+'.)  '+line +'\n' + todolist[line] + '\n\n'
+                    i+=1
 
-            await message.channel.send(post)
+                await client.wait_until_ready()
+                todo_channel = client.get_channel(1212207245245419530)
+                await todo_channel.send(post)
+            
             
     except Exception as error:
         print(error)
@@ -105,6 +141,6 @@ def runBot():
     async def on_message(message):
         if message.author == client.user: #don't respond to yourself, and get stuck in infinity.
             return
-        await process_message(message, message.content) #if you see a message -> see if its a git push.
+        await process_message(client, message.content) #if you see a message -> see if its a git push.
 
     client.run(discordToken)
